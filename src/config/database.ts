f8dda9f50812d95b2819801e433de3f0d87b6917
Marcos Zillig <marcos.zillig@gmail.com>
2020-05-03 01:27:01 -0300

    Emergency commit

---
+++ src/config/database.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  username: "root",
+  password: "root",
+  database: "graphql_blog_development",
+  host: "127.0.0.1",
+  dialect: "mysql",
+  operatorsAliases: false,
+};
--- src/interfaces/DbConnectionInterface.ts
+++ src/interfaces/DbConnectionInterface.ts
@@ -2,6 +2,5 @@ import { ModelsInterface } from './ModelsInterface';
 import * as Sequelize from "sequelize";
 
 export interface DbConnection extends ModelsInterface {
-
   sequelize: Sequelize.Sequelize
 }
